0ec04c47856b09ca2fdb0ddd90819a836a7c892f
Lukas <Lukas.Erekson@gmail.com>
2023-05-03 15:10:09 -0600

    [Issue #4] Begin writing pytest fixtures
    - Update pytest version to avoid errors running the tests.
    - Create pytest fixtures for the test database and app.
    - Rearrange file structure (mostly setup_db.py) to assist with build-up
      and tear-down of the test database.
    - Add tox.ini to configure flake8 warnings for unused __init__ imports,
      which just expose the API through the package.

--- budget_book_backend/__init__.py
+++ budget_book_backend/__init__.py
@@ -1,7 +1,7 @@
 """Budget Books: A Flask app for personal finance and budgeting."""
 __version__ = "0.1.0"
 
-from .backend import main
+from .backend import main, create_app
 
 if __name__ == "__main__":
     main(True)
--- tests/test_budget_book_backend.py
+++ tests/test_budget_book_backend.py
@@ -1,5 +1,34 @@
+from flask import Flask
+from sqlalchemy import Inspector, inspect
+
 from budget_book_backend import __version__
+from budget_book_backend.models.db_setup import DbSetup
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+def test_app_creation(app: Flask):
+    """Test whether the app gets created with the routes and database."""
+    route_keys: list[str] = list(app.blueprints.keys())
+
+    assert route_keys == [
+        "accounts",
+        "account_types",
+        "transactions",
+    ]
+
+    assert app.config.get("DATABASE") == "sqlite:///tests/test.db"
+
+
+def test_database_setup(use_test_db):
+    """Test that the database gets set up and that DbSetup has the
+    proper file and tables associated with it."""
+    assert str(DbSetup.engine.url) == "sqlite:///tests/test.db"
+
+    inspector: Inspector = inspect(DbSetup.engine)
+
+    tables: list[str] = inspector.get_table_names()
+
+    assert tables != []
